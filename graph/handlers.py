1808bef0362cfd5699c8eda5e0178ecfaa3ebe75
Alex Kurihara <alex.kurihara@gmail.com>
2018-11-30 23:21:02 -0500

    add main genre intermediate nodes

--- graph/handlers.py
+++ graph/handlers.py
@@ -1,5 +1,6 @@
 from tornado.web import HTTPError
 
+from database_utils import Session
 from graph import service as graph_service
 from handlers import BaseAPIHandler
 from spotify import service as spotify_service
@@ -14,9 +15,10 @@ class CreateTaxonomyGraphHandler(BaseAPIHandler):
             raise HTTPError(status_code=401)
 
         spotify_artists = await spotify_service.get_all_top_artists_for_user(access_token)
-        taxonomy_graph = graph_service.build_taxonomy_graph_from_spotify_artists(spotify_artists)
+        session = Session()
+        taxonomy_graph = graph_service.build_taxonomy_graph_from_spotify_artists(session, spotify_artists)
 
-        # response = taxonomy_graph.render_in_webgraphviz_format()
-        response = taxonomy_graph.render_as_json()
+        response = taxonomy_graph.render_in_webgraphviz_format()
+        # response = taxonomy_graph.render_as_json()
 
         return self.write(response)
--- graph/service.py
+++ graph/service.py
@@ -1,4 +1,6 @@
-from graph.models import TaxonomyGraph
+from collections import defaultdict
+
+from graph.models import Genre, TaxonomyGraph
 
 
 __all__ = [
@@ -6,30 +8,74 @@ __all__ = [
 ]
 
 
-def build_taxonomy_graph_from_spotify_artists(artists):
+def build_taxonomy_graph_from_spotify_artists(session, spotify_artists):
     taxonomy_graph = TaxonomyGraph()
 
-    for artist in artists:
-        artist_slug = artist.name.lower()
-
-        # Skip an artist if they already exist in the graph.
-        if artist_slug in taxonomy_graph:
+    for spotify_artist in spotify_artists:
+        # Skip an spotify_artist if they already exist in the graph.
+        if spotify_artist.name in taxonomy_graph:
             continue
 
-        artist_node = taxonomy_graph.add_node(artist_slug)
+        artist_node = taxonomy_graph.add_node(spotify_artist.name)
 
-        # Skip an artist if they do not have any associated genres.
-        if not artist.genres:
+        # Skip an spotify_artist if they do not have any associated genres.
+        if not spotify_artist.genres:
             continue
 
-        genre = artist.genres[0]
+        genre, subgenre = _choose_best_genre_and_subgenre_for_artist(session, spotify_artist)
 
         if genre not in taxonomy_graph:
             genre_node = taxonomy_graph.add_node(genre)
+            taxonomy_graph.add_edge(taxonomy_graph.get_root_node(), genre_node)
         else:
             genre_node = taxonomy_graph.get_node(genre)
 
-        taxonomy_graph.add_edge(taxonomy_graph.get_root_node(), genre_node)
-        taxonomy_graph.add_edge(genre_node, artist_node)
+        if subgenre not in taxonomy_graph:
+            subgenre_node = taxonomy_graph.add_node(subgenre)
+            taxonomy_graph.add_edge(genre_node, subgenre_node)
+        else:
+            subgenre_node = taxonomy_graph.get_node(subgenre)
+
+        taxonomy_graph.add_edge(subgenre_node, artist_node)
 
     return taxonomy_graph
+
+
+def _choose_best_genre_and_subgenre_for_artist(session, spotify_artist):
+    main_genres = _get_all_main_genres(session)
+    main_genre = _choose_main_genre_from_spotify_genres(main_genres, spotify_artist.genres)
+    subgenre = _choose_subgenre_from_spotify_genres(main_genre, spotify_artist.genres)
+
+    return main_genre, subgenre
+
+
+def _get_all_main_genres(session):
+    return session.query(Genre).all()
+
+
+def _choose_main_genre_from_spotify_genres(main_genres, spotify_genres):
+    main_genre_substring_matches = defaultdict(int)
+
+    for main_genre in main_genres:
+        for spotify_genre in spotify_genres:
+            if main_genre.spotify_name == spotify_genre:
+                return main_genre.spotify_name
+            elif main_genre.spotify_name in spotify_genre:
+                main_genre_substring_matches[main_genre.spotify_name] += 1
+
+    if main_genre_substring_matches:
+        main_genre = max(
+            main_genre_substring_matches.keys(),
+            key=lambda key: main_genre_substring_matches[key]
+        )
+        return main_genre
+
+    return 'Unknown'
+
+
+def _choose_subgenre_from_spotify_genres(main_genre, spotify_genres):
+    for spotify_genre in spotify_genres:
+        if main_genre != spotify_genre and main_genre in spotify_genre:
+            return spotify_genre
+
+    return spotify_genres[0]
