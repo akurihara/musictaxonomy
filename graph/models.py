fa38577b8e91ad0eed7876f12a34ab60d4c1e461
Alex Kurihara <alex.kurihara@gmail.com>
2019-03-26 23:16:48 -0400

    set up GenreNode, SubgenreNode, ArtistNode hierarchy

--- graph/models.py
+++ graph/models.py
@@ -59,18 +59,50 @@ class TaxonomyGraph(object):
 
         return new_node
 
-    def get_node(self, id):
+    def add_genre_node(self, id):
         if id in self.nodes:
-            return self.nodes[id]
-        else:
-            return None
+            raise Exception('Graph already contains node with ID {}'.format(id))
+
+        new_node = GenreNode(id)
+        self.nodes[id] = new_node
+
+        return new_node
+
+    def add_subgenre_node(self, id, name):
+        if id in self.nodes:
+            raise Exception('Graph already contains node with ID {}'.format(id))
+
+        new_node = SubgenreNode(id, name)
+        self.nodes[id] = new_node
+
+        return new_node
+
+    def add_artist_node(self, id, name):
+        if id in self.nodes:
+            raise Exception('Graph already contains node with ID {}'.format(id))
+
+        new_node = ArtistNode(id, name)
+        self.nodes[id] = new_node
+
+        return new_node
+
+    def get_node(self, id):
+        return self.nodes[id] if id in self.nodes else None
 
     def add_edge(self, first_node, second_node):
-        '''
-        first_node = self.get_node(first_node_id) # or self.add_node(first_node_id)
-        second_node = self.get_node(second_node_id) # or self.add_node(second_node_id)
-        '''
         first_node.add_neighbor(second_node)
+
+    def add_genre_to_subgenre_edge(self, genre_node, subgenre_node):
+        self.add_edge(genre_node, subgenre_node)
+        subgenre_node.set_genre(genre_node.id)
+
+    def add_genre_to_artist_edge(self, genre_node, artist_node):
+        self.add_edge(genre_node, artist_node)
+        artist_node.set_genre(genre_node.id)
+
+    def add_subgenre_to_artist_edge(self, subgenre_node, artist_node):
+        self.add_edge(subgenre_node, artist_node)
+        artist_node.set_genre(subgenre_node.genre)
 
     def render_as_json(self):
         return {
@@ -109,14 +141,58 @@ class Node(object):
     def render_as_json(self):
         return {'id': self.id}
 
-    def get_edges_in_webgraphviz_format(self):
-        return [
-            self._render_edge_in_webgraphviz_format(neighbor)
-            for neighbor in self.get_neighbors()
-        ]
-
-    def _render_edge_in_webgraphviz_format(self, neighbor):
-        return '"{}" -> "{}"'.format(self.id, neighbor.id)
-
     def __str__(self):
         return str(self.id) + ' connected_to: ' + str([neighbor.id for neighbor in self.neighbors])
+
+
+class GenreNode(Node):
+
+    __slots__ = ['id', 'name', 'neighbors']
+
+    def __init__(self, id):
+        super().__init__(id)
+        self.name = id
+
+    def render_as_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+        }
+
+
+class SubgenreNode(Node):
+
+    __slots__ = ['id', 'genre', 'name', 'neighbors']
+
+    def __init__(self, id, name):
+        super().__init__(id)
+        self.name = name
+        self.genre = None
+
+    def set_genre(self, genre):
+        self.genre = genre
+
+    def render_as_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+        }
+
+
+class ArtistNode(Node):
+
+    __slots__ = ['id', 'genre', 'name', 'neighbors']
+
+    def __init__(self, id, name):
+        super().__init__(id)
+        self.name = name
+        self.genre = None
+
+    def set_genre(self, genre):
+        self.genre = genre
+
+    def render_as_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+        }
--- graph/service.py
+++ graph/service.py
@@ -10,41 +10,52 @@ __all__ = [
 
 def build_taxonomy_graph_from_spotify_artists(session, spotify_artists):
     taxonomy_graph = TaxonomyGraph()
+    main_genres = _get_all_main_genres(session)
 
     for spotify_artist in spotify_artists:
         # Skip an spotify_artist if they already exist in the graph.
-        if spotify_artist.name in taxonomy_graph:
+        if spotify_artist.id in taxonomy_graph:
             continue
-
-        artist_node = taxonomy_graph.add_node(spotify_artist.name)
 
         # Skip an spotify_artist if they do not have any associated genres.
         if not spotify_artist.genres:
             continue
 
-        genre, subgenre = _choose_best_genre_and_subgenre_for_artist(session, spotify_artist)
+        artist_node = taxonomy_graph.add_artist_node(
+            spotify_artist.id,
+            spotify_artist.name,
+        )
+
+        genre, subgenre = _choose_best_genre_and_subgenre_for_artist(
+            session,
+            spotify_artist,
+            main_genres,
+        )
 
         if genre not in taxonomy_graph:
-            genre_node = taxonomy_graph.add_node(genre)
+            genre_node = taxonomy_graph.add_genre_node(genre)
             taxonomy_graph.add_edge(taxonomy_graph.get_root_node(), genre_node)
         else:
             genre_node = taxonomy_graph.get_node(genre)
 
-        if subgenre not in taxonomy_graph:
-            subgenre_node = taxonomy_graph.add_node(subgenre)
-            taxonomy_graph.add_edge(genre_node, subgenre_node)
+        if subgenre is None:
+            taxonomy_graph.add_genre_to_artist_edge(genre_node, artist_node)
         else:
-            subgenre_node = taxonomy_graph.get_node(subgenre)
+            if subgenre not in taxonomy_graph:
+                subgenre_node = taxonomy_graph.add_subgenre_node(subgenre, subgenre.title())
+                taxonomy_graph.add_genre_to_subgenre_edge(genre_node, subgenre_node)
+            else:
+                subgenre_node = taxonomy_graph.get_node(subgenre)
 
-        taxonomy_graph.add_edge(subgenre_node, artist_node)
+            taxonomy_graph.add_subgenre_to_artist_edge(subgenre_node, artist_node)
 
     return taxonomy_graph
 
 
-def _choose_best_genre_and_subgenre_for_artist(session, spotify_artist):
-    main_genres = _get_all_main_genres(session)
+def _choose_best_genre_and_subgenre_for_artist(session, spotify_artist, main_genres):
     main_genre = _choose_main_genre_from_spotify_genres(main_genres, spotify_artist.genres)
-    subgenre = _choose_subgenre_from_spotify_genres(main_genre, spotify_artist.genres)
+    spotify_artist_genres = _filter_out_main_genres(spotify_artist.genres, main_genres)
+    subgenre = _choose_subgenre_from_spotify_genres(main_genre, spotify_artist_genres)
 
     return main_genre, subgenre
 
@@ -59,9 +70,9 @@ def _choose_main_genre_from_spotify_genres(main_genres, spotify_genres):
     for main_genre in main_genres:
         for spotify_genre in spotify_genres:
             if main_genre.spotify_name == spotify_genre:
-                return main_genre.spotify_name
+                return main_genre.display_name
             elif main_genre.spotify_name in spotify_genre:
-                main_genre_substring_matches[main_genre.spotify_name] += 1
+                main_genre_substring_matches[main_genre.display_name] += 1
 
     if main_genre_substring_matches:
         main_genre = max(
@@ -73,7 +84,15 @@ def _choose_main_genre_from_spotify_genres(main_genres, spotify_genres):
     return 'Unknown'
 
 
+def _filter_out_main_genres(spotify_arist_genres, main_genres):
+    main_genre_spotify_names = {main_genre.spotify_name for main_genre in main_genres}
+    return [genre for genre in spotify_arist_genres if genre not in main_genre_spotify_names]
+
+
 def _choose_subgenre_from_spotify_genres(main_genre, spotify_genres):
+    if len(spotify_genres) == 0:
+        return None
+
     for spotify_genre in spotify_genres:
         if main_genre != spotify_genre and main_genre in spotify_genre:
             return spotify_genre
