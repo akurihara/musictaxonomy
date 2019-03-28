d72c4c3dab273be19c1eb0add7d345a4516a312f
Alex Kurihara <alex.kurihara@gmail.com>
2019-03-27 23:24:15 -0400

    update graph building logic

--- graph/service.py
+++ graph/service.py
@@ -21,50 +21,54 @@ def build_taxonomy_graph_from_spotify_artists(session, spotify_artists):
         if not spotify_artist.genres:
             continue
 
-        artist_node = taxonomy_graph.add_artist_node(
-            spotify_artist.id,
-            spotify_artist.name,
-        )
-
-        genre, subgenre = _choose_best_genre_and_subgenre_for_artist(
+        main_genre_name, subgenre_name = _choose_best_main_genre_and_subgenre_names_for_artist(
             session,
             spotify_artist,
             main_genres,
         )
 
-        if genre not in taxonomy_graph:
-            genre_node = taxonomy_graph.add_genre_node(genre)
-            taxonomy_graph.add_edge(taxonomy_graph.get_root_node(), genre_node)
-        else:
-            genre_node = taxonomy_graph.get_node(genre)
+        if main_genre_name is None or subgenre_name is None:
+            continue
 
-        if subgenre is None:
-            taxonomy_graph.add_genre_to_artist_edge(genre_node, artist_node)
-        else:
-            if subgenre not in taxonomy_graph:
-                subgenre_node = taxonomy_graph.add_subgenre_node(subgenre, subgenre.title())
-                taxonomy_graph.add_genre_to_subgenre_edge(genre_node, subgenre_node)
-            else:
-                subgenre_node = taxonomy_graph.get_node(subgenre)
+        artist_node = taxonomy_graph.add_artist_node(
+            spotify_artist.id,
+            spotify_artist.name,
+        )
 
+        main_genre_node = taxonomy_graph.get_node(main_genre_name)
+        subgenre_node = taxonomy_graph.get_node(subgenre_name)
+
+        if main_genre_node and subgenre_node:
+            taxonomy_graph.add_subgenre_to_artist_edge(subgenre_node, artist_node)
+        elif main_genre_node:
+            subgenre_node = taxonomy_graph.add_subgenre_node(subgenre_name, subgenre_name.title())
+            taxonomy_graph.add_genre_to_subgenre_edge(main_genre_node, subgenre_node)
+            taxonomy_graph.add_subgenre_to_artist_edge(subgenre_node, artist_node)
+        elif subgenre_node:
+            taxonomy_graph.add_subgenre_to_artist_edge(subgenre_node, artist_node)
+        else:
+            main_genre_node = taxonomy_graph.add_genre_node(main_genre_name)
+            subgenre_node = taxonomy_graph.add_subgenre_node(subgenre_name, subgenre_name.title())
+            taxonomy_graph.add_edge(taxonomy_graph.get_root_node(), main_genre_node)
+            taxonomy_graph.add_genre_to_subgenre_edge(main_genre_node, subgenre_node)
             taxonomy_graph.add_subgenre_to_artist_edge(subgenre_node, artist_node)
 
     return taxonomy_graph
 
 
-def _choose_best_genre_and_subgenre_for_artist(session, spotify_artist, main_genres):
-    main_genre = _choose_main_genre_from_spotify_genres(main_genres, spotify_artist.genres)
+def _choose_best_main_genre_and_subgenre_names_for_artist(session, spotify_artist, main_genres):
+    main_genre_name = _choose_main_genre_name_from_spotify_genres(main_genres, spotify_artist.genres)
     spotify_artist_genres = _filter_out_main_genres(spotify_artist.genres, main_genres)
-    subgenre = _choose_subgenre_from_spotify_genres(main_genre, spotify_artist_genres)
+    subgenre_name = _choose_subgenre_name_from_spotify_genres(main_genre_name, spotify_artist_genres)
 
-    return main_genre, subgenre
+    return main_genre_name, subgenre_name
 
 
 def _get_all_main_genres(session):
     return session.query(Genre).all()
 
 
-def _choose_main_genre_from_spotify_genres(main_genres, spotify_genres):
+def _choose_main_genre_name_from_spotify_genres(main_genres, spotify_genres):
     main_genre_substring_matches = defaultdict(int)
 
     if 'pop' in spotify_genres and 'edm' in spotify_genres:
@@ -84,6 +88,18 @@ def _choose_main_genre_from_spotify_genres(main_genres, spotify_genres):
         )
         return main_genre
 
+    for spotify_genre in spotify_genres:
+        if 'indie' in spotify_genre:
+            return 'Rock'
+        if 'house' in spotify_genre:
+           return 'Electronic'
+        if 'funk' in spotify_genre:
+            return 'R&B'
+        if 'soul' in spotify_genre:
+            return 'R&B'
+        if 'adult standards' in spotify_genre:
+            return 'Pop'
+
     return 'Unknown'
 
 
@@ -92,7 +108,7 @@ def _filter_out_main_genres(spotify_arist_genres, main_genres):
     return [genre for genre in spotify_arist_genres if genre not in main_genre_spotify_names]
 
 
-def _choose_subgenre_from_spotify_genres(main_genre, spotify_genres):
+def _choose_subgenre_name_from_spotify_genres(main_genre, spotify_genres):
     if len(spotify_genres) == 0:
         return None
 
