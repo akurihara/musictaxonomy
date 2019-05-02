f355ad465715b441466dc944d702de3c73b8220b
Alex Kurihara <alex.kurihara@gmail.com>
2019-05-01 22:49:10 -0400

    add docstrings to auth handlers

--- musictaxonomy/auth/handlers.py
+++ musictaxonomy/auth/handlers.py
@@ -13,6 +13,16 @@ from settings import HOST, SPOTIFY_CLIENT_ID
 class LoginHandler(BaseAPIHandler):
 
     async def get(self):
+        """
+        Before using Music Taxonomy, a user must authenticate with Spotify's Oauth. This
+        endpoint is the first step in that authentication flow. We redirect to Spotify's login
+        page, passing query parameters to identify our application and specify which scopes the
+        application requires. We also pass a redirect URL parameter, which Spotify will use once
+        the user logs in successfully.
+
+        For more information, see:
+        https://developer.spotify.com/documentation/general/guides/authorization-guide
+        """
         access_token = self.get_access_token()
         is_access_token_valid = await auth_service.is_access_token_valid(access_token)
 
@@ -37,6 +47,17 @@ class LoginHandler(BaseAPIHandler):
 class OauthCallbackHandler(BaseAPIHandler):
 
     async def get(self):
+        """
+        This endpoint is the second and final step of the authentication flow. At this point
+        the user has logged in successfully to Spotify, and the user has been redirected back to
+        Music Taxonomy with an authorization code. We exchange this authorization code for an
+        access token by making a request to Spotify, passing our application's credentials. Once
+        we receive the access token, we redirect to the IndexHandler so the user can start using
+        the application.
+
+        For more information, see:
+        https://developer.spotify.com/documentation/general/guides/authorization-guide
+        """
         authorization_code = self.get_argument('code')
         access_token_response = await spotify_client.get_access_token(
             authorization_code
