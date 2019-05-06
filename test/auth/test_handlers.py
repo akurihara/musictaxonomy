75013dedfee4d865d0c14e9d2cd39aa7809acb41
Alex Kurihara <alex.kurihara@gmail.com>
2019-05-05 23:30:21 -0400

    add one more auth test

--- test/auth/test_handlers.py
+++ test/auth/test_handlers.py
@@ -39,7 +39,11 @@ class LoginHandlerTest(AsyncHTTPTestCase):
     @vcr.use_cassette('test/auth/cassettes/test_get_with_valid_access_token.yml', ignore_localhost=True)
     def test_get_with_valid_access_token(self):
         headers = {
-            "Cookie": "AccessToken=2|1:0|10:1557112070|11:AccessToken|208:QlFERHdTaF9FTmJPMnZ0T1A2bEg2Z0Mtdjk4QzZndjdZV1RBZERZcFp1TTM5SlNmVVBkV2RybGFuX1JoQW1fZlZHajV0djR3dE5fbkxzNjNMRVBvZ2ttRjNEY1dJUmpHNVJzV1VEUTRlVjVKS0lVQV9fSlNOS0dpVU9GbHFpdHFYSHgyRHhnN2VRNlduXzN5S1RoZ2RpSk5IeHR2|d960f7c5da188ca13c0e1307e9c91fe81ea9d877308acc90da076738143b70c5",
+            "Cookie": "AccessToken=2|1:0|10:1557112070|11:AccessToken|208:QlFERHdTaF9FTmJPMn"
+                      "Z0T1A2bEg2Z0Mtdjk4QzZndjdZV1RBZERZcFp1TTM5SlNmVVBkV2RybGFuX1JoQW1fZlZ"
+                      "HajV0djR3dE5fbkxzNjNMRVBvZ2ttRjNEY1dJUmpHNVJzV1VEUTRlVjVKS0lVQV9fSlNO"
+                      "S0dpVU9GbHFpdHFYSHgyRHhnN2VRNlduXzN5S1RoZ2RpSk5IeHR2|d960f7c5da188ca1"
+                      "3c0e1307e9c91fe81ea9d877308acc90da076738143b70c5",
         }
         response = self.fetch(
             path='/login',
@@ -55,6 +59,33 @@ class LoginHandlerTest(AsyncHTTPTestCase):
         parsed_url = parse.urlparse(response.headers['Location'])
         self.assertEqual(parsed_url.netloc, '')
         self.assertEqual(parsed_url.path, '/')
+
+    @vcr.use_cassette('test/auth/cassettes/test_get_with_invalid_access_token.yml', ignore_localhost=True)
+    def test_get_with_invalid_access_token(self):
+        headers = {
+            "Cookie": "AccessToken=invalid",
+        }
+        response = self.fetch(
+            path='/login',
+            method='GET',
+            headers=headers,
+            follow_redirects=False,
+        )
+
+        # Verify the response code.
+        self.assertEqual(response.code, 302)
+
+        # Verify the redirect host and path.
+        parsed_url = parse.urlparse(response.headers['Location'])
+        self.assertEqual(parsed_url.netloc, 'accounts.spotify.com')
+        self.assertEqual(parsed_url.path, '/authorize')
+
+        # Verify the redirect query parameters.
+        parsed_query_string = dict(parse.parse_qsl(parsed_url.query))
+        self.assertEqual(parsed_query_string['response_type'], 'code')
+        self.assertEqual(parsed_query_string['scope'], 'user-top-read')
+        self.assertEqual(parsed_query_string['client_id'], SPOTIFY_CLIENT_ID)
+        self.assertTrue(parsed_query_string['redirect_uri'].endswith('/callback/oauth'))
 
 
 class OauthCallbackHandlerTest(AsyncHTTPTestCase):
