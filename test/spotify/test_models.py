cd862f034f4730dd13f85f25b16d4d08e6f23d6a
Alex Kurihara <alex.kurihara@gmail.com>
2019-05-20 23:50:06 -0400

    add test for spotify models

---
+++ test/spotify/test_models.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+
+from musictaxonomy.spotify.models import SpotifyArtist, SpotifyUser
+
+
+class SpotifyArtistTest(TestCase):
+
+    def test_conversion_to_string(self):
+        spotify_artist = SpotifyArtist(
+            id='3TVXtAsR1Inumwj472S9r4',
+            name='Drake',
+            genres=['hip hop', 'pop rap', 'rap'],
+        )
+
+        self.assertEqual(str(spotify_artist), '3TVXtAsR1Inumwj472S9r4')
+
+
+class SpotifyUserTest(TestCase):
+
+    def test_conversion_to_string(self):
+        spotify_user = SpotifyUser(
+            id='1220628328',
+            display_name='Alex Kuriahra',
+        )
+
+        self.assertEqual(str(spotify_user), '1220628328')
