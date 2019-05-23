ed9c64b9bc69508cea39d3d5b14614d2434f36f4
Alex Kurihara <alex.kurihara@gmail.com>
2019-05-23 00:53:30 -0400

    add tests for graph service

---
+++ test/graph/test_service.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+from musictaxonomy.database import Session
+from musictaxonomy.graph import service as graph_service
+from musictaxonomy.spotify.models import SpotifyArtist, SpotifyUser
+
+
+class BuildTaxonomyGraphTest(TestCase):
+
+    def test_build_taxonomy_graph_with_unknown_main_genre(self):
+        session = Session()
+        spotify_user = SpotifyUser(
+            id='1220628328',
+            display_name='Alex Kuriahra',
+        )
+        spotify_artist = SpotifyArtist(
+            id='3TVXtAsR1Inumwj472S9r4',
+            name='Drake',
+            genres=['foo'],
+        )
+
+        taxonomy_graph = graph_service.build_taxonomy_graph(session, spotify_user, [spotify_artist])
+
+        self.assertIsNotNone(taxonomy_graph.get_node('Unknown'))
